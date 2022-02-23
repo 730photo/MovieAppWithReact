e77d084e4174a8c3d4ca15ea90302b5cbc2507ec
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2022-02-23 17:26:37 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+display: flex;
+flex-direction: row;
+background-color: black;
+color: white;
+padding: 10px;
+`;
+
 function App() {
-  return (
-    <div className="App">
-      Yessir!
-    </div>
-  );
+  return <Container>
+    <Header>Movie App with React</Header>
+    Yessir!</Container>;
 }
 
 export default App;
